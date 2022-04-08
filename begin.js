3d0658b70aca7d8b6b5abcfbe57de801d868abbf
Thomas McElmeel <81656327+helloItIsThomas@users.noreply.github.com>
2022-04-07 22:20:04 -0400

    Add files via upload

---
+++ begin.js
@@ -0,0 +1,12 @@
+
+function setup(){
+  createCanvas(windowWidth,windowHeight);
+  background('#FFFFFF');
+}
+
+function draw(){
+  background('#FFFFFF');
+  fill('#111111');
+  lineDrawKey();
+  shapeCopiesKey();
+}
